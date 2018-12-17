d89a4b1b3fe6983eb38278626fb3ad14ef5fa478
SulyvanD <sulyvan.debraize@net-c.com>
2018-12-17 14:57:17 +0100

    Exercice completé

---
+++ Variables - Exercice 6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables___Exercice_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nom, prenom;
+            int annee;
+
+            Console.WriteLine("Quel est votre nom ?");
+            nom = Console.ReadLine();
+            Console.WriteLine("Quel est votre prénom ?");
+            prenom = Console.ReadLine();
+            Console.WriteLine("Quelle est votre année de naissance ?");
+            annee = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Fiche de renseignements:" + Environment.NewLine + "Nom: " + nom + Environment.NewLine +
+                "Prénom: " + prenom + Environment.NewLine + "Age: " + (DateTime.Now.Year - annee));
+        }
+    }
+}
